9b533306f7e543b35fa6cc7c1e82b25005003f22
Alok Kumar <alokk13799@gmail.com>
2023-02-25 03:37:39 +0530

    config remove

--- src/firebase.js
+++ src/firebase.js
@@ -3,9 +3,21 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
-import dotenv from "dotenv";
-dotenv.config();
+// const API_KEY = process.env.API_KEY;
+// const AUTH_DOMAIN = process.env.AUTH_DOMAIN;
+// const PROJECT_ID = process.env.PROJECT_ID;
+// const STORAGE_BUCKET = process.env.STORAGE_BUCKET;
+// const MESSAGING_SENDER_ID = process.env.MESSAGING_SENDER_ID;
+// const APP_ID = process.env.APP_ID;
 
+// const firebaseConfig = {
+// 	apiKey: API_KEY,
+// 	authDomain: AUTH_DOMAIN,
+// 	projectId: PROJECT_ID,
+// 	storageBucket: STORAGE_BUCKET,
+// 	messagingSenderId: MESSAGING_SENDER_ID,
+// 	appId: APP_ID,
+// };
 
 const firebaseConfig = {
 	apiKey: "AIzaSyCJDPKYQJhJVZ3kbADxSycJ8_bVIYOBT8I",
